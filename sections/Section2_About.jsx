8ac67be85760b9f7c257c3386f39906a63ec2901
Samarth <splangote_b20@ce.vjti.ac.in>
2022-12-21 22:10:00 +0530

    no changes

--- sections/Section2_About.jsx
+++ sections/Section2_About.jsx
@@ -37,6 +37,103 @@ const About = () => (
       />
     </motion.div>
   </section>
+
 );
 
 export default About;
+// import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
+// import Grid from '@mui/material/Grid';
+// import Avatar from '@mui/material/Avatar';
+// import Typography from '@mui/material/Typography';
+// import styles from '../styles';
+// const StyledPaper = styled(Paper)(({ theme }) => ({
+//   // backgroundColor: theme.palette.mode === "light",
+//   backgroundColor: "#1A2238",
+//   ...theme.typography.body2,
+//   padding: theme.spacing(2),
+//   maxWidth: 270,
+//   color: theme.palette.text.primary,
+// }));
+
+// const message = `Truncation should be conditionally applicable on this long line of text
+//  as this is a much longer line than what the container can support. `;
+
+// export default function About() {
+//   return (
+//     <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }} className="BOX">
+//       <StyledPaper
+//         sx={{
+//           my: 1,
+//           mx: 'auto',
+//           p: 2,
+//         }}
+//       >
+//         <Grid container wrap="nowrap" spacing={2}>
+//           <Grid item>
+//             <Avatar>A</Avatar>
+//           </Grid>
+//           <Grid item xs>
+//             <Typography style={{ color: "white" }}>{message}</Typography>
+//           </Grid>
+//         </Grid>
+//       </StyledPaper>
+//       <StyledPaper
+//         sx={{
+//           my: 1,
+//           mx: 'auto',
+//           p: 2,
+//         }}
+//       >
+//         <Grid container wrap="nowrap" spacing={2}>
+//           <Grid item>
+//             <Avatar>E</Avatar>
+//           </Grid>
+//           <Grid item xs>
+//             <Typography style={{ color: "white" }}>{message}</Typography>
+//           </Grid>
+//         </Grid>
+//       </StyledPaper>
+//       <StyledPaper
+//         sx={{
+//           my: 1,
+//           mx: 'auto',
+//           p: 2,
+//         }}
+//       >
+//         <Grid container wrap="nowrap" spacing={2}>
+//           <Grid item>
+//             <Avatar>G</Avatar>
+//           </Grid>
+//           <Grid item xs>
+//             <Typography style={{ color: "white" }}>{message}</Typography>
+//           </Grid>
+//         </Grid>
+//       </StyledPaper>
+//       <StyledPaper
+//         sx={{
+//           my: 1,
+//           mx: 'auto',
+//           p: 2,
+//         }}
+//       >
+//         <Grid container wrap="nowrap" spacing={2}>
+//           <Grid item>
+//             <Avatar>F</Avatar>
+//           </Grid>
+//           <Grid item xs>
+//             <Typography style={{ color: "white" }}>{message}</Typography>
+//           </Grid>
+//         </Grid>
+//       </StyledPaper>
+//     </Box>
+//   );
+// }
+// @media screen and (min-width:900px){
+//   .BOX{
+//     display:flex;
+//     flex-direction:row;
+//   }
+//   }
