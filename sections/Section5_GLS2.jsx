2f5688ccd547d2f77485d6abfac8bb388dbd576f
Akshay <afalke007@gmail.com>
2022-12-22 01:27:16 +0530

    section6world removed

--- sections/Section5_GLS2.jsx
+++ sections/Section5_GLS2.jsx
@@ -81,36 +81,33 @@ const GlsAPJ = () => (
         {/* <TypingText title="| Insights of Technovanza" /> */}
         <TitleText title={<><span className="font-bold text-4xl">Dr. A. P. J. Abdul Kalam</span> <br /><span className="text-2xl font-bold">at Techno<span style={{ color: '#26A699' }}>Vanza</span></span> </>} />
         <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
-        <div>  <div className={`${styles.flexCenter} flex-row`}>
-        <div
-          
-        >
-          <p className="font-bold text-[20px] text-white">
-          <div>
-          <div class="youtube">
-            <span class="arrow"></span>
+          <div>  <div className={`${styles.flexCenter} flex-row`}>
+            <div>
+              <p className="font-bold text-[20px] text-white">
+                <div>
+                  <div className="youtube">
+                    <span className="arrow" />
+                  </div>
+                </div>
+              </p>
+            </div>
+            <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]" style={{ marginTop: "15px" }}>
+              Find a world that suits you and you want to enter
+            </p>
           </div>
-        </div>
-          </p>
-        </div>
-        <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]"   style={{marginTop:"15px"}}>
-          Find a world that suits you and you want to enter
-        </p>
-      </div>
-      </div>
-      <div>  <div className={`${styles.flexCenter} flex-row`}>
-        <div
-          
-        >
-          <p className="font-bold text-[20px] text-white">
-          <div style={{fontSize:"40px",fontWeight:"20px"}}>#</div>
-          </p>
-        </div>
-        <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]"   style={{marginTop:"15px"}}>
-          Find a world that suits you and you want to enter
-        </p>
-      </div>
-      </div>
+          </div>
+          <div>  
+            <div className={`${styles.flexCenter} flex-row`}>
+              <div>
+                <p className="font-bold text-[20px] text-white">
+                  <div style={{ fontSize: '40px', fontWeight: '20px', marginLeft: '10px' }}>#</div>
+                </p>
+              </div>
+              <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]" style={{ marginTop: '15px', marginLeft: '35px' }}>
+                Find a world that suits you and you want to enter
+              </p>
+            </div>
+          </div>
         </div>
       </motion.div>
     </motion.div>
