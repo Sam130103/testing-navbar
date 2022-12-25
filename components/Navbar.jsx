dc8b81bead693ce7f0a807df88898423c39832b0
Akshay <afalke007@gmail.com>
2022-12-25 05:33:58 +0530

    gls headings

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,8 @@
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import Head from 'next/head';
-import Loginout from '../pages/loginout'
-import { useSession, signIn, signOut } from "next-auth/react"
+import Loginout from '../pages/loginout';
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Navbar() {
   const { data: session } = useSession();
   return (
@@ -71,29 +71,32 @@ export default function Navbar() {
                   <span className="nav__name">Gallery</span>
                 </a>
               </li>
-
               <li className="nav__item">
                 <a href="/about" className="nav__link">
                   <i className="bx bx-message-square-detail nav__icon" />
                   <span className="nav__name">About</span>
                 </a>
               </li>
-              <li className="nav__item" style={{ marginRight: '15px' }}>
-                <i className="bx bx-message-square-detail nav__icon" />
-                {
-                  session ?
-                    <>
-                      <button onClick={() => { signOut() }} className="loginout">
-                        <a className='nav_item nav_link  nav_name'>Logout</a>
-                      </button>
-                    </>
-                    :
-
-                    <button onClick={() => signIn()} className="loginout" >
-                      Login
-                    </button>
-                }
+              {
+                session ?
+                <>
+                     <li className="nav__item">
+                      <a onClick={() => { signOut(); }}  className="nav__link">
+                        <i className="bx bx-message-square-detail nav__icon" />
+                        <span className="nav__name">Logout</span>
+                      </a>
+                     </li>
+                </>
+                :
+                <>
+                     <li className="nav__item">
+                      <a onClick={() => signIn()} className="nav__link">
+                      <i className="bx bx-message-square-detail nav__icon" />
+                      <span className="nav__name">Login</span>
+                    </a>
               </li>
+                </>
+              }
             </ul>
           </motion.div>
         </nav>
--- pages/gls/index.js
+++ pages/gls/index.js
@@ -4,10 +4,19 @@
 
 // import { NavbarCopy } from '../../components';
 // import { gls } from '../../utils/gls.js';
+import { Navbar } from '../../components';
+import GlsHeading from '../../components/gls/GlsHeading';
 import Glsmap from '../../components/gls/Glsmap';
 
 const Gls = () => (
-  <Glsmap />
+  <div>
+    <Navbar />
+    {/* //   <div className="font-bold text-white">
+  //     hello
+  //   </div> */}
+    <Glsmap />
+    <GlsHeading />
+  </div>
 );
 
 export default Gls;
